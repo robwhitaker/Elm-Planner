94bbaa3c599f01b5d51b418b74f677831d1fc372
Rob Whitaker <robjameswhitaker@gmail.com>
2015-05-03 00:29:01 -0400

    Completed base functionality. Cleaned up code. * Removed unnecessary / redundant fields from Item type. * Generally improved update function. * Keyboard controls now enable / disable appropriately. * Expanding / hiding child nodes now works.     - Hidden nodes can no longer be selected. * Additional keyboard shortcuts added for Renaming & Expanding nodes.

--- TreePlanner.elm
+++ TreePlanner.elm
@@ -26,32 +26,30 @@ type alias State = {
     rootNode : Tree Item,
     projectTitle : String,
     sidebarSize : Float,
-    selectedId : Int
+    selectedId : Int,
+    renamingCurrentNode : Bool
 }
 
 type alias Item = {
     title : String,
     content : String,
-    expanded : Bool,
-    selected : Bool,
-    renaming : Bool
+    expanded : Bool
 }
 
 newItem : Item 
 newItem = {
     title = "new",
     content = "",
-    expanded = False,
-    selected = False,
-    renaming = False
+    expanded = True
     }
 
 emptyModel : State
 emptyModel = {
-    rootNode = T.Node { newItem | selected <- True } [T.Node newItem [] 1, T.Node newItem [T.Node newItem [] 3] 2] 0, 
+    rootNode = T.newNode T.Empty newItem [], 
     projectTitle = "Untitled",
     sidebarSize = 0.2,
-    selectedId = 0
+    selectedId = 0,
+    renamingCurrentNode = False
     }
 
 ---------- UPDATE ----------
@@ -62,116 +60,123 @@ type Action
     | MoveSelection SelectionMovement
     | RenameItem String
     | UpdateItem String
-    | RenamingItem Int
+    | RenamingItem (Maybe Int)
     | DeleteItem
     | MoveNode NodeMovement
-    | ToggleExpanded Int
+    | ToggleExpanded (Maybe Int)
     | NoOp
 
 type SelectionMovement = Up | Down
 update : Action -> State -> State
-update action state = case action of
+update action state = let
+            selectableNodes = T.mapN (\(T.Node value children id) -> if not value.expanded then (T.Node value [] id) else (T.Node value children id)) state.rootNode
+        in case action of
         NewItem -> let
-            getNodeFromId = (Maybe.withDefault (T.dummyNode newItem) << flip T.nodeById state.rootNode)
-            parent = getNodeFromId state.selectedId
+            parent = T.nodeByIdWithDefault (T.dummyNode newItem) state.selectedId state.rootNode
             child = T.newNode state.rootNode newItem []
             newTree = T.addChildTo parent child state.rootNode
                       |> T.moveNode T.Lift child
             in { state | rootNode <- newTree, selectedId <- T.id child }
 
-        SelectItem selectedId ->
-            { state | rootNode <- T.mapToNodeById (T.valueM (\item -> { item | selected <- True })) selectedId 
-                               <| T.map (T.valueM (\item -> { item | selected <- False, renaming <- False })) state.rootNode, 
-                      selectedId <- Debug.log "ehh" selectedId }
+        SelectItem selectedId -> { state | selectedId <- selectedId }
 
         MoveSelection dir -> case dir of
-            Up -> { state | selectedId <- T.id <| Maybe.withDefault (T.Node newItem [] 0) <| List.head <| List.reverse <| Utils.takeWhile ((/=) state.selectedId << T.id) (T.flatten state.rootNode) }
-            Down -> { state | selectedId <- T.id <| Maybe.withDefault (T.Node newItem [] state.selectedId) <| List.head <| List.drop 1 <| Utils.dropWhile ((/=) state.selectedId << T.id) (T.flatten state.rootNode) }
+            Up -> { state | selectedId <- T.id <| Maybe.withDefault (T.Node newItem [] 0) <| List.head <| List.reverse <| Utils.takeWhile ((/=) state.selectedId << T.id) (T.flatten selectableNodes) }
+            Down -> { state | selectedId <- T.id <| Maybe.withDefault (T.Node newItem [] state.selectedId) <| List.head <| List.drop 1 <| Utils.dropWhile ((/=) state.selectedId << T.id) (T.flatten selectableNodes) }
 
         RenameItem newName -> 
-            { state | rootNode <- T.mapToNodeById (T.valueM (\item -> { item | title <- newName, renaming <- False, selected <- True })) 
-                                                   state.selectedId 
-                                                   state.rootNode }
+            { state | rootNode <- T.mapToNodeById (\item -> { item | title <- newName }) state.selectedId state.rootNode, renamingCurrentNode <- False }
 
         UpdateItem newContent ->
-            { state | rootNode <- T.mapToNodeById (T.valueM (\item -> { item | content <- newContent })) state.selectedId state.rootNode }
+            { state | rootNode <- T.mapToNodeById (\item -> { item | content <- newContent }) state.selectedId state.rootNode }
 
-        RenamingItem selectedId ->
-            { state | rootNode <- T.mapToNodeById (T.valueM (\item -> { item | renaming <- True, selected <- True })) selectedId 
-                               <| T.map (T.valueM (\item -> { item | renaming <- False, selected <- False })) state.rootNode }
+        RenamingItem selectedId -> case selectedId of
+            Just sId -> { state | selectedId <- sId, renamingCurrentNode <- True }
+            Nothing  -> { state | renamingCurrentNode <- True }
 
-        DeleteItem ->
-            { state | rootNode <- T.removeNodeById state.selectedId state.rootNode }
+        DeleteItem -> let
+                previousNode = Maybe.withDefault (T.dummyNode newItem) <| List.head <| List.reverse 
+                               <| Utils.takeWhile ((/=) (T.nodeByIdWithDefault (T.dummyNode newItem) state.selectedId state.rootNode)) (T.flatten selectableNodes)
+                prevId = if T.id previousNode < 0 then 0 else T.id previousNode
+            in { state | rootNode <- T.removeNodeById state.selectedId state.rootNode, selectedId <- prevId }
 
-        MoveNode movement ->
-            { state | rootNode <- T.moveNodeById movement state.selectedId state.rootNode }
+        MoveNode movement -> case movement of
+            T.Lower -> let 
+                newState = { state | rootNode <- T.moveNodeById movement state.selectedId state.rootNode }
+                parent = T.parent (T.nodeByIdWithDefault state.rootNode state.selectedId newState.rootNode) newState.rootNode |> Maybe.withDefault state.rootNode
+                in { newState | rootNode <- T.mapToNodeById (\item -> { item | expanded <- True }) (T.id parent) newState.rootNode }
 
-        ToggleExpanded selectedId ->
-            { state | rootNode <- T.mapToNodeById (T.valueM (\item -> { item | expanded <- not item.expanded })) selectedId state.rootNode }
+            _       -> { state | rootNode <- T.moveNodeById movement state.selectedId state.rootNode }
+
+        ToggleExpanded selectedId -> 
+            Maybe.withDefault state.selectedId selectedId
+            |> (\sId -> { state | rootNode <- T.mapToNodeById (\item -> { item | expanded <- not item.expanded }) sId state.rootNode })
 
         _ -> state
 
 ---------- VIEW ----------
 
-view : State -> (Int, Int) -> Html
-view state (w, h) = let
+view : State -> (Int, Int) -> Bool -> Html
+view state (w, h) keysEnabled = let
     w' = toFloat w
     h' = toFloat h
-    getNodeFromId = (Maybe.withDefault (T.dummyNode newItem) << flip T.nodeById state.rootNode)
     in div [style [("height", "100%"),("border", "1px solid black")]] [
         div [style [("height", toString (h' * 0.06) ++ "px"), ("background-color", "grey")]] [text "top bar"],
         div [style [("display", "inline-block"), ("width", toString (w' * 0.22) ++ "px"), ("height", toString (h' * 0.88) ++ "px"), ("border", "1px solid black")],
-            onClick keyboardControls.address True
-        ] [lazy2 treeToHtmlTree state state.rootNode],
+            onClick keyboardControlsEnabled.address True
+        ] [lazy3 treeToHtmlTree state keysEnabled state.rootNode],
         div [style [("display", "inline-block"), ("width", toString (w' * 0.01) ++ "px")]] [],
         div [style [("display", "inline-block"), ("width", toString (w' * 0.76) ++ "px"), ("height", toString (h' * 0.88 - 5) ++ "px"), ("position", "absolute"), ("right", "1"), ("border", "1 px solid black")],
-            onClick keyboardControls.address False,
-            onBlur keyboardControls.address True
+            onClick keyboardControlsEnabled.address False,
+            onBlur keyboardControlsEnabled.address True
         ]
             [textarea [style [("display", "inline-block"), ("float","right"), ("width", "100%"), ("height", "100%"), ("border", "none"), ("resize", "none")], 
                        placeholder "editor",
-                       value (T.value (getNodeFromId state.selectedId)).content,
-                       on "change" targetValue (Signal.message uiInput.address << UpdateItem)
+                       value (T.value (T.nodeByIdWithDefault (T.dummyNode newItem) state.selectedId state.rootNode)).content,
+                       on "change" targetValue (Signal.message uiInput.address << UpdateItem),
+                       onBlur keyboardControlsEnabled.address True,
+                       onFocus keyboardControlsEnabled.address False
                       ] []]
     ]
 
-treeToHtmlTree : State -> Tree Item -> Html
-treeToHtmlTree state (T.Node item children id') = let
-        liContent = if item.renaming 
+treeToHtmlTree : State -> Bool -> Tree Item -> Html
+treeToHtmlTree state keysEnabled (T.Node item children id') = let
+        liContent = if state.renamingCurrentNode && id' == state.selectedId
                     then 
                         input [   
                             value item.title,
                             id ("node-" ++ toString id'), 
-                            on "change" targetValue (Signal.message uiInput.address << RenameItem), 
-                            onBlur keyboardControls.address True,
-                            onFocus keyboardControls.address False,
-                            onEnter targetValue (Signal.message uiInput.address << RenameItem)
+                            onEnter targetValue (Signal.message uiInput.address << RenameItem),
+                            on "blur" targetValue (Signal.message uiInput.address << RenameItem),
+                            onFocus keyboardControlsEnabled.address False
                         ] []
                     else 
                         div [
-                            classList [("selected", id' == state.selectedId)],
+                            classList [("selected-focused", id' == state.selectedId && keysEnabled), ("selected-unfocused", id' == state.selectedId && (not keysEnabled))],
                             onClick uiInput.address (SelectItem id'), 
-                            onDoubleClick uiInput.address (RenamingItem id')
+                            onDoubleClick uiInput.address (RenamingItem (Just id'))
                         ] [text item.title]
         in ul [] [
-            li [] <| liContent :: (List.map (lazy2 treeToHtmlTree state) children)]
+            li [classList [("hidden", not item.expanded)]] <| liContent :: (if item.expanded then List.map (lazy3 treeToHtmlTree state keysEnabled) children else [])]
 
 onEnter : Json.Decoder a -> (a -> Signal.Message) -> Attribute
 onEnter decoder f = on "keydown"
                     (Json.object2 (\code val -> if code == 13 then Ok val else Err "") keyCode decoder)
-                    (\(Ok value) -> f <| Debug.log "val" value)
+                    (\result -> case result of
+                        Ok value  -> f value
+                        Err _ -> Signal.message errBox.address ()
+                    )
 
 ---------- INPUTS ----------
 
 main : Signal Html
-main = Signal.map2 view state Window.dimensions
+main = Signal.map3 view state Window.dimensions keyboardControlsEnabled'
 
 state : Signal State
 state = Signal.foldp update emptyModel <| Signal.merge uiInput.signal keyboardInput
 
---TODO: add SampleOn so this won't interfere with renaming nodes
 keyboardInput : Signal Action
-keyboardInput = Signal.map2 (,) keyboardControlsEnabled Keyboard.keysDown
+keyboardInput = Signal.map2 (,) keyboardControlsEnabled' Keyboard.keysDown
     |> Signal.dropRepeats
     |> Signal.map (\(enabled, keypresses) -> if not enabled then NoOp else
         case (Set.toList keypresses) of
@@ -181,6 +186,8 @@ keyboardInput = Signal.map2 (,) keyboardControlsEnabled Keyboard.keysDown
             [17, 40] -> MoveNode T.ShiftDown
             [13, 17] -> NewItem
             [17, 46] -> DeleteItem
+            [32]     -> ToggleExpanded Nothing
+            [13]     -> RenamingItem Nothing
             [38]     -> MoveSelection Up
             [40]     -> MoveSelection Down
             _        -> NoOp
@@ -188,22 +195,28 @@ keyboardInput = Signal.map2 (,) keyboardControlsEnabled Keyboard.keysDown
     |> Signal.filter ((/=) NoOp) NoOp
 
 
-keyboardControlsEnabled : Signal Bool
-keyboardControlsEnabled = Signal.foldp (always << identity) True keyboardControls.signal
+keyboardControlsEnabled' : Signal Bool
+keyboardControlsEnabled' = Signal.merge keyboardControlsEnabled.signal 
+    <| Signal.map (\action ->
+            case action of
+                RenamingItem _ -> False
+                _ -> True
+        ) uiInput.signal
 
----------- WIRING ----------
+---------- MAILBOXES ----------
 
 uiInput : Signal.Mailbox Action
 uiInput = Signal.mailbox NoOp
 
-keyboardControls : Signal.Mailbox Bool
-keyboardControls = Signal.mailbox True
+keyboardControlsEnabled : Signal.Mailbox Bool
+keyboardControlsEnabled = Signal.mailbox True
+
+errBox : Signal.Mailbox ()
+errBox = Signal.mailbox ()
+
+---------- PORTS ----------
 
 port focus : Signal String
-port focus = let
-    needsFocus action = case action of
-        RenamingItem id -> True
-        _               -> False
-
-    toSelector (RenamingItem id) = "#node-" ++ toString id
-    in uiInput.signal |> Signal.filter needsFocus (RenamingItem -1) |> Signal.map toSelector
+port focus = Signal.filter ((/=) "") "" 
+          <| Signal.dropRepeats 
+          <| Signal.map (\s -> if s.renamingCurrentNode then "#node-" ++ toString s.selectedId else "") state
